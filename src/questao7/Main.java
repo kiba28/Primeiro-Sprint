f396ff264c6960c98e9c5bdeee11a00ecc262b0f
José Layrton <jose.ribeio.pb@compasso.com.br>
2022-02-05 10:53:33 -0300

    Atualizada a questão 7, para fazer uso de POO com a classe Perguntas.

--- src/questao7/Main.java
+++ src/questao7/Main.java
@@ -1,5 +1,6 @@
 package questao7;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,40 +9,41 @@ public class Main {
 		String nome;
 		int acertos = 0;
 		int erros = 0;
-		String resposta;
+		ArrayList<Perguntas> perguntas = new ArrayList<Perguntas>();
 		Scanner scan = new Scanner(System.in);
-		String[] perguntas = { "De quem é a famosa frase “Penso, logo existo”?",
-				"De onde é a invenção do chuveiro elétrico?", "Quantas casas decimais tem o número pi?",
-				"Qual é o menor pais do mundo?", "Qual é o maior pais do mundo?",
-				"Atualmente, quantos elementos químicos a tabela periódica possui?",
-				"Qual o número mínimo de jogadores numa partida de futebol?",
-				"Qual pais tem a maior expectativa de vida?", "Qual país tem a menor expectaviva de vida?",
-				"Quanto tempo a luz do Sol demora para chegar à Terra?" };
 
-		String[] respostas = { "Descartes", "Brasil", "Infinitas", "Vaticano", "Rússia", "118", "7", "Japão",
-				"Serra Leoa", "8 minutos", };
+		perguntas.add(new Perguntas("De quem é a famosa frase “Penso, logo existo”?", "Descartes"));
+		perguntas.add(new Perguntas("De onde é a invenção do chuveiro elétrico?", "Brasil"));
+		perguntas.add(new Perguntas("Quantas casas decimais tem o número pi?", "Infinitas"));
+		perguntas.add(new Perguntas("Qual é o menor pais do mundo?", "Vaticano"));
+		perguntas.add(new Perguntas("Qual é o maior pais do mundo?", "Rússia"));
+		perguntas.add(new Perguntas("Atualmente, quantos elementos químicos a tabela periódica possui?", "118"));
+		perguntas.add(new Perguntas("Qual o número mínimo de jogadores numa partida de futebol?", "7"));
+		perguntas.add(new Perguntas("Qual pais tem a maior expectativa de vida?", "Japão"));
+		perguntas.add(new Perguntas("Qual país tem a menor expectaviva de vida?", "Serra Leoa"));
+		perguntas.add(new Perguntas("Quanto tempo a luz do Sol demora para chegar à Terra?", "8 minutos"));
 
 		System.out.println("Digite seu nome: ");
 		nome = scan.next();
 
 		for (int i = 0; i < 2; i++) {
-			System.out.println(i + 1 + "ª pegunta: \n" + perguntas[i]);
+			System.out.println(i + 1 + "ª pegunta: \n" + perguntas.get(i).getPergunta());
 			System.out.print("Resposta: ");
-			resposta = scan.next();
-			if (respostas[i].equals(resposta)) {
+			String resposta = scan.next();
+			if (perguntas.get(i).getResposta().equalsIgnoreCase(resposta)) {
 				System.out.println("Acertou!!!\n");
 				acertos++;
 			} else {
 				System.out.println("Você errou! :(");
-				System.out.println("A resposta certá é " + respostas[i]);
+				System.out.println("A resposta certá é " + perguntas.get(i).getResposta());
 				System.out.println();
 				erros++;
 			}
 		}
 		System.out.println(
-				"Olá " + nome + ". \nVocê acertou: " + acertos + " de 10." + "\nE errou: " + erros + " de 10.");
-
+				"Olá " + nome + ". \nVocê acertou: " + acertos + "\nE errou: " + erros);
+		
 		scan.close();
 	}
-	
+
 }
---
+++ src/questao7/Perguntas.java
@@ -0,0 +1,28 @@
+package questao7;
+
+public class Perguntas {
+	String pergunta;
+	String resposta;
+	
+	public Perguntas(String pergunta, String resposta) {
+		this.pergunta = pergunta;
+		this.resposta = resposta;
+	}
+
+	public String getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+	
+}
